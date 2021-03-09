06521569e05004139a17dcab64e94942ca22e2a8
hero <gust4f3rn@gmail.com>
2021-03-09 19:05:39 -0300

    api date seted

--- frontend/src/components/Location/index.tsx
+++ frontend/src/components/Location/index.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from 'react';
+import api from '../../api/api';
 import { Container } from './styles';
 
+interface WeatherInfo{
+  location: {
+    country: "string",
+    lat: "number",
+    lon: "number",
+    localtime: "number",
+    name: "string",
+    region: "string",
+  },
+  current: {
+    condition: {
+      icon: 'string',
+      text: 'string'
+    }
+  }
+}
+
+
 function Location() {
+
+  const [ weather, setWeather ] = useState<WeatherInfo>()
+
+  var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+
+  useEffect(() => {
+    api.get('https://api.weatherapi.com/v1/current.json?key=dc546a4fbf4d45508cf212117210403&q=Curitiba&aqi=no').then(response => {
+      setWeather(response.data)
+    })
+  }, [])
+
   return (
     <Container>
-      <h1>Curitiba, BR</h1>
-      <p>Tuesday, 9 March</p>
+      <h1>{weather?.location.name}, {weather?.location.country}</h1>
+      <p>{n}, {weather?.location.localtime}</p>
     </Container>
   );
 };
